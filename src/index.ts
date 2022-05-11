9c9ffbcefe5d08bf39ba214908b34998444b2596
Ruben Oliveira <mail.roiveira@gmail.com>
2022-05-11 16:24:47 +0100

    Minor change to api export

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,26 @@ import {IApiConfig} from "./model/IApiConfig";
 import {ApiService} from "./apiService/ApiService";
 import {ApiProxy} from "./apiProxy/ApiProxy";
 
+const exposeApi = (apiConfig: IApiConfig, api: Record<string, Function>, Transport: Object) => {
+    const apiService =  new ApiService(apiConfig, api);
+    // @ts-ignore
+    apiService.setOutboundFn(new Transport(apiService.getInboundFn()).outboundFn);
+
+    // TODO: allow for destroy
+};
+
+const createProxy = (apiConfig: IApiConfig, Transport: Object) => {
+    console.log("creating proxy...");
+
+    const apiProxy = new ApiProxy(apiConfig);
+    // @ts-ignore
+    apiProxy.setOutboundFn(new Transport(apiProxy.getInboundFn()).outboundFn);
+
+    return apiProxy.get();
+}
+
+
 export const proxycom = {
-    exposeApi(apiConfig: IApiConfig, api: Record<string, Function>, Transport: Object) {
-        const apiService =  new ApiService(apiConfig, api);
-        // @ts-ignore
-        apiService.setOutboundFn(new Transport(apiService.getInboundFn()).outboundFn);
-
-        // TODO: allow for destroy
-    },
-    createProxy(apiConfig: IApiConfig, Transport: Object) {
-        const apiProxy = new ApiProxy(apiConfig);
-        // @ts-ignore
-        apiProxy.setOutboundFn(new Transport(apiProxy.getInboundFn()).outboundFn);
-
-        return apiProxy.get();
-    }
+    exposeApi,
+    createProxy
 }
