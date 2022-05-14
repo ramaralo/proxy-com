cf55bf471cb206db0a82b46f1de5d416edd5c2af
Ruben Oliveira <mail.roiveira@gmail.com>
2022-05-14 01:11:03 +0100

    Update utils to use process transport

--- integrationSpecs/processTransportUtils.js
+++ integrationSpecs/processTransportUtils.js
@@ -1,31 +1,15 @@
 const path = require("path");
 const {fork} = require('child_process');
-
+const {processTransport} = require("./../dist/transports/nodejs/process");
 const {proxycom} = require("../dist");
 
 const apiConfig = {props: ["foo"]};
-
-/*
-Expose API to child
- */
-function getTransportForService(child) {
-    return class TransportAdapter {
-        constructor(inboundFn) {
-            child.on("message", inboundFn);
-        }
-
-        outboundFn(payload) {
-            child.send(payload);
-        }
-    }
-}
-
 
 module.exports = {
     start: (fooSpy, callFooWith, resolvedSpy) => {
         const child = fork(path.resolve(__dirname,'./child-process'));
 
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve)=> {
             child.on("message", (message) => {
                 if(message.action === "returnValue") {
                     resolvedSpy(message.value);
@@ -38,7 +22,7 @@ module.exports = {
                 foo: fooSpy
             }
 
-            proxycom.exposeApi(apiConfig, service, getTransportForService(child));
+            proxycom.exposeApi(apiConfig, service, processTransport.getForParentProcess(child));
 
             child.send({action: "callFoo", value: callFooWith});
         })
