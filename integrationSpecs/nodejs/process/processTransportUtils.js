4cdaf27aa593533eb01c9954c5377858bad5da16
Ruben Oliveira <mail.roiveira@gmail.com>
2022-05-16 12:53:52 +0100

    Some JSDocs

--- integrationSpecs/nodejs/process/processTransportUtils.js
+++ integrationSpecs/nodejs/process/processTransportUtils.js
@@ -5,7 +5,21 @@ const {proxycom} = require("../../../dist");
 
 const apiConfig = {props: ["foo"]};
 
+/**
+ * Utility to help the test execution of multi-process scenario.
+ * @type {{start: (function(*, *, *): Promise<unknown>)}}
+ */
 module.exports = {
+    /**
+     * Forks a child process (see file childProcess) and exposes an api.
+     * Once forked, it sends a message to child process to call an api method with a specific argument.
+     * Child process will then send a message with the returned value.
+     *
+     * @param fooSpy The method to be exposed on the service
+     * @param callFooWith The value to be passed
+     * @param resolvedSpy The function to call with the value received by child
+     * @returns {Promise<*>} A promise that resolves once all parent-child interaction is done
+     */
     start: (fooSpy, callFooWith, resolvedSpy) => {
         const child = fork(path.resolve(__dirname,'./child-process'));
 
