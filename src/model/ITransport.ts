cb79b71123f35d0bbe6b1082f55086663aa8e031
Ruben Oliveira <mail.roiveira@gmail.com>
2022-05-24 17:49:49 +0100

    Update README and docs update ts process transport types

--- src/model/ITransport.ts
+++ src/model/ITransport.ts
@@ -1,12 +1,19 @@
 import { IRequestPayload } from "./IRequestPayload";
 import { IResponsePayload } from "./IResponsePayload";
 
+/**
+ * The constructor interface.
+ *
+ * A Transport can be implemented with a class or a function as it will be called with the new keyword.
+ * Typically, you will need a Transport for the exposed API and a Transport for the Proxy. Since a Transport will probably
+ * depend on the
+ */
 export interface ITransportConstructor {
   new (
     inboundFn: (payload: IRequestPayload | IResponsePayload) => void
   ): ITransport;
 }
 
-interface ITransport {
+export interface ITransport {
   outboundFn: (payload: IRequestPayload | IResponsePayload) => void;
 }
--- src/transports/nodejs/process.ts
+++ src/transports/nodejs/process.ts
@@ -1,32 +1,36 @@
-import {ChildProcess} from "child_process";
-import {IResponsePayload} from "../../model/IResponsePayload";
-import {IRequestPayload} from "../../model/IRequestPayload";
+import { ChildProcess } from "child_process";
+import { IResponsePayload } from "../../model/IResponsePayload";
+import { IRequestPayload } from "../../model/IRequestPayload";
 import * as Process from "process";
-
+import { ITransport, ITransportConstructor } from "../../model/ITransport";
 
 export const processTransport = {
-    getForParentProcess: (child: ChildProcess) => {
-        return class TransportAdapter {
-            constructor(inboundFn: () => void) {
-                child.on("message", inboundFn);
-            }
+  getForParentProcess: (child: ChildProcess): ITransportConstructor => {
+    return class TransportAdapter {
+      constructor(
+        inboundFn: (payload: IRequestPayload | IResponsePayload) => void
+      ) {
+        child.on("message", inboundFn);
+      }
 
-            outboundFn(payload: IResponsePayload | IRequestPayload) {
-                child.send(payload);
-            }
-        }
-    },
-    getForChildProcess: (process: typeof Process) => {
-        return class TransportAdapter {
-            constructor(inboundFn: () => void) {
-                // @ts-ignore
-                process.on("message", inboundFn);
-            }
+      outboundFn(payload: IResponsePayload | IRequestPayload): void {
+        child.send(payload);
+      }
+    };
+  },
+  getForChildProcess: (process: typeof Process): ITransportConstructor => {
+    return class TransportAdapter {
+      constructor(
+        inboundFn: (payload: IRequestPayload | IResponsePayload) => void
+      ) {
+        // @ts-ignore
+        process.on("message", inboundFn);
+      }
 
-            outboundFn(payload: IResponsePayload | IRequestPayload) {
-                // @ts-ignore
-                process.send(payload);
-            }
-        }
-    }
-}
+      outboundFn(payload: IResponsePayload | IRequestPayload) {
+        // @ts-ignore
+        process.send(payload);
+      }
+    };
+  },
+};
